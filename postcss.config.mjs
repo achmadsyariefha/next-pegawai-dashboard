fb4610de9defcf1c859c7636d0b4b586009be38b
Achmad Syarief Hidayatullah Amiruddin <33272892+achmadsyariefha@users.noreply.github.com>
2025-09-02 11:07:36 +0800

    seventh commit
    change file type

--- postcss.config.mjs
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-
-import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
-
-export default {
-  plugins: [tailwindcss, autoprefixer],
-}
